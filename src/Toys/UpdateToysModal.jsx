6de99d4db714170e251b47d4ca55a4f95b4b19cd
mehedi1802hasan <mehedi1802hasan@gmail.com>
2023-05-21 11:46:24 +0600

    edit updatetoymodal

--- src/Toys/UpdateToysModal.jsx
+++ src/Toys/UpdateToysModal.jsx
@@ -9,18 +9,15 @@ const handleAddToy=event=>{
     event.preventDefault();
     
     const form=event.target;
-    const productName=form.productName.value;
-    const sellerName=form.sellerName.value;
-    const sellerEmail=form.sellerEmail.value;
-    const imgUrl=form.imgUrl.value;
+   
     const price=form.price.value;
-    const rating=form.rating.value;
+    
     const quantity=form.quantity.value;
-    const subCategory=form.subCategory.value;
+    
     const description=form.description.value;
     const _id=form._id.value;
     const addToy={
-        productName,sellerName,sellerEmail,imgUrl,price,rating,quantity,_id,subCategory,description
+    price,quantity,_id,description
     }
     console.log(addToy)
     fetch(`http://localhost:5000/updateToy/${_id}`,{
@@ -54,43 +51,14 @@ const handleAddToy=event=>{
     
   <form onSubmit={handleAddToy} className=" card-body">
       <div className='grid grid-cols-1 gap-8 md:grid-cols-2'>
-        <div className="form-control">
-       <label className="label">
-            <span className="label-text">Product-Name</span>
-          </label>
-          <input type="text" name='productName' placeholder="Enter your Product name" className="input input-bordered" />
-        </div>
-        <div className="form-control">
-       <label className="label">
-            <span className="label-text">Seller Name</span>
-          </label>
-          <input type="text" name='sellerName' placeholder="Enter Seller Name" className="input input-bordered" defaultValue={user?.displayName} />
-        </div>
-        <div className="form-control">
-       <label className="label">
-            <span className="label-text">Seller Email</span>
-          </label>
-          <input type="text" name="sellerEmail" placeholder="Enter seller email" className="input input-bordered" defaultValue={user?.email}/>
-        </div>
-        
-        <div className="form-control">
-       <label className="label">
-            <span className="label-text">ImageURL</span>
-          </label>
-          <input type="text" name="imgUrl" placeholder="Enter product image url" className="input input-bordered" />
-        </div>
+       
         <div className="form-control">
        <label className="label">
             <span className="label-text">Price</span>
           </label>
           <input type="text" name='price' placeholder="enter product price" className="input input-bordered" defaultValue="$"/>
         </div>
-        <div className="form-control">
-       <label className="label">
-            <span className="label-text">Rating</span>
-          </label>
-          <input type="text" name='rating' placeholder="Enter product ratings" className="input input-bordered" />
-        </div>
+      
         
         <div className=" form-control">
        <label className="label">
@@ -105,18 +73,7 @@ const handleAddToy=event=>{
           </label>
           <input type="number" placeholder='enter product quantity' name='quantity'  className="input input-bordered" />
         </div>
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Sub-Category</span>
-          </label>
-          <select name='subCategory' className='h-12'>
-  <option value="science">Science Toy</option>
-  <option value="math">Math Toy</option>
-  <option value="english">English Toy</option>
- 
-</select>
-         
-        </div>
+      
         
         <div className="form-control">
        <label className="label">
